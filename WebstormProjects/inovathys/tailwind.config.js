bd2d2ecfdf0bce7e2c2fa3f5edf3da3773244a4b
Eduardo Henrique <eduardohcvm@gmail.com>
2024-09-16 19:01:58 -0300

    segundo commit, tradução da tela de login

--- WebstormProjects/inovathys/tailwind.config.js
+++ WebstormProjects/inovathys/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Certifique-se de que isso cobre todos os arquivos que contêm classes Tailwind
+  ],
   theme: {
     extend: {},
   },
