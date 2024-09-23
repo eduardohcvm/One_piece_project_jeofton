0efc422e0270a9e5e6bfeeef0e224f87a09b1c76
Eduardo Henrique <eduardohcvm@gmail.com>
2024-09-23 15:17:06 -0300

    branch_resolvida

--- WebstormProjects/inovathys/WebstormProjects/inovathys/src/App.js
+++ WebstormProjects/inovathys/WebstormProjects/inovathys/src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import './componentes/login'
 import LoginForm from "./componentes/login";
 import Home from "./componentes/home";
+import Dashboard from "./componentes/comodo_info";
+import User_info from "./componentes/user_info";
 
 function App() {
   return (
     <div className="App">
+
     <Home/>
     </div>
   );
